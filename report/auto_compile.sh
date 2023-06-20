94d9b5cec8eeae734ac4f7e1e675783290d2a0e7
Eva van Houten <opensource@evavh.net>
2023-06-20 11:57:47 +0200

    Setup and write first part of report

---
+++ report/auto_compile.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#SUCCESS=false
+
+#ls $1.tex | entr -rc bash -c "mv $1.aux $1.aux.back; pdflatex -shell-escape -halt-on-error $1 && ./open_once.sh $1 $SUCCESS && SUCCESS=true"
+ls $1.tex | entr -rc bash -c "mv $1.aux $1.aux.back; lualatex -shell-escape -halt-on-error $1"
