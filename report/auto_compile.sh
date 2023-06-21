ca8c8a8078e3e5119d5f844d3caa7f42c302a078
Eva van Houten <opensource@evavh.net>
2023-06-21 12:51:23 +0200

    Add implementation + references to report

--- report/auto_compile.sh
+++ report/auto_compile.sh
@@ -3,4 +3,4 @@
 #SUCCESS=false
 
 #ls $1.tex | entr -rc bash -c "mv $1.aux $1.aux.back; pdflatex -shell-escape -halt-on-error $1 && ./open_once.sh $1 $SUCCESS && SUCCESS=true"
-ls $1.tex | entr -rc bash -c "mv $1.aux $1.aux.back; lualatex -shell-escape -halt-on-error $1"
+ls $1.tex | entr -rc bash -c "mv $1.aux $1.aux.back; pdflatex -shell-escape -halt-on-error $1; biber $1; pdflatex $1"
